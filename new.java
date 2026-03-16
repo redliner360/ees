594dce51301344504d01c5df796d73c8605d25d4
redliner360 <vvce23ise0095@vvce.ac.in>
2026-03-16 13:52:08 +0530

    Update print statement in Hello class

--- new.java
+++ new.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Java Build!");
+        System.out.println("Hello from Jenkins Java Build!  sdkmn vks ");
     }
 }
