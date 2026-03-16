1fe0d19e77499342095d1832a2217f0c2b061bce
redliner360 <vvce23ise0095@vvce.ac.in>
2026-03-16 13:56:03 +0530

    Update output message in Hello class

--- new.java
+++ new.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Java Build!  sdkmn vks ");
+        System.out.println("Hello from Jenkins Java Build!  s x fcvsdkmn vks ");
     }
 }
